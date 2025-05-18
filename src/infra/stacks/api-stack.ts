82ac073b049cf667919c6938ec5d8b450c094b48
rooster man <roostermanred@gmail.com>
2025-05-18 15:34:41 -0500

    add put and delete to stack

--- src/infra/stacks/api-stack.ts
+++ src/infra/stacks/api-stack.ts
@@ -15,5 +15,7 @@ export class ApiStack extends Stack {
 
     spacesResource.addMethod('GET', props.spacesLambdaIntegration)
     spacesResource.addMethod('POST', props.spacesLambdaIntegration)
+    spacesResource.addMethod('PUT', props.spacesLambdaIntegration)
+    spacesResource.addMethod('DELETE', props.spacesLambdaIntegration)
   }
 }
